0466b6c10a0f24f33e014d1b1efd2a4f75f1e319
Igor Lapin <raynor73@gmail.com>
2023-02-19 14:54:47 +0000

    Add MainActivity

---
+++ app/src/main/java/ilapin/template/MainActivity.kt
@@ -0,0 +1,11 @@
+package ilapin.template
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
