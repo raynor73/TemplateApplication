5a35b1e7f88ab64af651f310d775b6f569faf0c4
Igor Lapin <raynor73@gmail.com>
2023-02-19 13:51:20 +0000

    Implement instrumented test files update

--- init.py
+++ init.py
@@ -2,6 +2,24 @@
 
 import sys
 import shutil
+import os
+from pathlib import Path
+
+def updateAppIdInFile(filePath, placeholderAppId, appId):
+	file = open(filePath, "r", encoding="utf-8")
+	fileContent = file.read()
+	file.close()
+
+	fileContent = fileContent.replace(placeholderAppId, appId)
+
+	filePathOld = filePath + ".old"
+	shutil.move(filePath, filePathOld)
+
+	file = open(filePath, "w", encoding="utf-8")
+	file.write(fileContent)
+	file.close()
+
+	os.remove(filePathOld)
 
 if (len(sys.argv) <= 1):
 	print("Usage:", sys.argv[0], "APPLICATION_ID")
@@ -14,8 +32,12 @@ applicationId = sys.argv[1]
 applicationIdParts = applicationId.split(".")
 applicationIdBeginning = applicationIdParts[:-1]
 applicationIdPathBeginning = "/".join(applicationIdBeginning)
-applicationIdPathBeginning.mkdir(parents = True, exist_ok = True)
+Path(applicationIdPathBeginning).mkdir(parents = True, exist_ok = True)
 shutil.move(
 	appAndroidTestRootPath + templateId.replace(".", "/"),
 	appAndroidTestRootPath + applicationId.replace(".", "/")
 )
+
+appAndroidTestKtFiles = list(Path(appAndroidTestRootPath).rglob("*.kt"))
+for filePath in appAndroidTestKtFiles:
+	updateAppIdInFile(filePath, templateId, applicationId)
