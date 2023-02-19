9734dec54dd94f658a483b8bebcdedf5a14faefd
Igor Lapin <raynor73@gmail.com>
2023-02-19 15:14:00 +0000

    Implement updating files and dirs in the root of app module

--- init.py
+++ init.py
@@ -21,27 +21,16 @@ def updateAppIdInFile(filePath, placeholderAppId, appId):
 
 	os.remove(filePathOld)
 
-def updateDirStructureUsingAppId(rootPath, placeholderAppId, appId):
-	applicationIdParts = appId.split(".")
-	applicationIdBeginning = applicationIdParts[:-1]
-	applicationIdPathBeginning = "/".join(applicationIdBeginning)
-	Path(rootPath + applicationIdPathBeginning).mkdir(parents = True, exist_ok = True)
-	shutil.move(
-		rootPath + placeholderAppId.replace(".", "/"),
-		rootPath + appId.replace(".", "/")
-	)
-
 if (len(sys.argv) <= 1):
 	print("Usage:", sys.argv[0], "APPLICATION_ID")
 	exit()
 
 appAndroidTestRootPath = "./app/src/androidTest/java/"
 appTestRootPath = "./app/src/test/java/"
+appRootPath = "./app/src/main/java/"
 placeholderAppId = "ilapin.template"
 applicationId = sys.argv[1]
 
-#updateDirStructureUsingAppId(appAndroidTestRootPath, placeholderAppId, applicationId)
-#updateDirStructureUsingAppId(appTestRootPath, placeholderAppId, applicationId)
 shutil.move(
 	appAndroidTestRootPath + placeholderAppId.replace(".", "/"),
 	appAndroidTestRootPath + applicationId.replace(".", "/")
@@ -50,6 +39,10 @@ shutil.move(
 	appTestRootPath + placeholderAppId.replace(".", "/"),
 	appTestRootPath + applicationId.replace(".", "/")
 )
+shutil.move(
+	appRootPath + placeholderAppId.replace(".", "/"),
+	appRootPath + applicationId.replace(".", "/")
+)
 
 appAndroidTestKtFiles = list(Path(appAndroidTestRootPath).rglob("*.kt"))
 for filePath in appAndroidTestKtFiles:
@@ -57,6 +50,10 @@ for filePath in appAndroidTestKtFiles:
 
 appTestKtFiles = list(Path(appTestRootPath).rglob("*.kt"))
 for filePath in appTestKtFiles:
+	updateAppIdInFile(str(filePath), placeholderAppId, applicationId)
+
+appKtFiles = list(Path(appRootPath).rglob("*.kt"))
+for filePath in appKtFiles:
 	updateAppIdInFile(str(filePath), placeholderAppId, applicationId)
 
 updateAppIdInFile("./app/build.gradle", placeholderAppId, applicationId)
